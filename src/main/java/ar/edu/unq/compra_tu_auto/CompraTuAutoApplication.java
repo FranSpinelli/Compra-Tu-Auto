7e377439ba2825f128c113ab692546526a89c53d
franciscos <franciscos@pagotic.com>
2025-08-29 01:02:10 -0300

    repo init

---
+++ src/main/java/ar/edu/unq/compra_tu_auto/CompraTuAutoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.compra_tu_auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraTuAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompraTuAutoApplication.class, args);
+    }
+
+}
