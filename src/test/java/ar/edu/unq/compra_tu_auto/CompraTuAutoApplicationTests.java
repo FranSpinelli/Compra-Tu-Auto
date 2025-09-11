44f1595b6c219e8c8d17cd48e4387bb6ffd14dcb
JuanG10 <juangaray463@gmail.com>
2025-09-11 18:40:25 -0300

    Revert "Feature/bookmark abm (#6)"
    This reverts commit 5c5317fda0cce7dae07d7d9753c46672a35c345f.
    
    Reverts bookmark ABM debido a que se mergeó a master y debía ir a develop

---
+++ src/test/java/ar/edu/unq/compra_tu_auto/CompraTuAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.compra_tu_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraTuAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
