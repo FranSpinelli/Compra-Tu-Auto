ddb25dd92fecdc17babd59e69931972411528d28
JuanG10 <44150310+JuanG10@users.noreply.github.com>
2025-09-11 18:49:19 -0300

    Revert "Feature/bookmark abm (#6)" merge into master
    This reverts commit 5c5317fda0cce7dae07d7d9753c46672a35c345f.
    
    Reverts bookmark ABM debido a que se mergeó a master y debía ir a develop

--- src/main/java/ar/edu/unq/compra_tu_auto/controller/DTO/error/ValidationErrorResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.compra_tu_auto.controller.DTO.error;
-
-import java.util.Map;
-
-public class ValidationErrorResponseDTO extends ErrorResponseDTO<Map<String, String>> {
-    public ValidationErrorResponseDTO(Map<String, String> errors) {
-        super(errors);
-    }
-}
