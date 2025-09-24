a861fcbb379acbfef9ef57ab5d8c33412a6ef81d
Francisco Spinelli <franciscospinelli98@gmail.com>
2025-09-24 00:08:01 -0300

    Feature
    Se agregan constraints a nivel bbdd, para que no se puedan insterar:
    - 2 modelos de autos diferentes, con mismo nombre de compañia y modelo.
    - 2 colores iguales para un mismo modelo de auto.
    - 2 años de fabricacion identicos para un mismo modelo de auto.

--- src/main/java/ar/edu/unq/compra_tu_auto/repository/sqlRepository/entities/CarModelEntity.java
+++ src/main/java/ar/edu/unq/compra_tu_auto/repository/sqlRepository/entities/CarModelEntity.java
@@ -5,7 +5,12 @@ import jakarta.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "car_models")
+@Table(
+        name = "car_models",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"company_name", "model_name"})
+        }
+)
 public class CarModelEntity {
 
     @Id
@@ -17,12 +22,24 @@ public class CarModelEntity {
     private String modelName;
 
     @ElementCollection
-    @CollectionTable(name = "car_model_manufacturing_years", joinColumns = @JoinColumn(name = "car_model_id"))
+    @CollectionTable(
+            name = "car_model_manufacturing_years",
+            joinColumns = @JoinColumn(name = "car_model_id"),
+            uniqueConstraints = @UniqueConstraint(
+                    columnNames = {"car_model_id", "manufacturing_year"}
+            )
+    )
     @Column(name = "manufacturing_year")
     private List<Integer> manufacturingYears;
 
     @ElementCollection
-    @CollectionTable(name = "car_model_colors", joinColumns = @JoinColumn(name = "car_model_id"))
+    @CollectionTable(
+            name = "car_model_colors",
+            joinColumns = @JoinColumn(name = "car_model_id"),
+            uniqueConstraints = @UniqueConstraint(
+                    columnNames = {"car_model_id", "color_hexadecimal_code"}
+            )
+    )
     @Column(name = "color_hexadecimal_code")
     private List<String> colors;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,6 @@
 -- Car Models
 INSERT INTO car_models (company_name, model_name) VALUES
                                                       ('Peugeot', '208'),
-                                                      ('Peugeot', '208'),
                                                       ('Fiat', 'Argo'),
                                                       ('Fiat', 'Cronos'),
                                                       ('Toyota', 'Etios'),
@@ -32,11 +31,10 @@ INSERT INTO car_model_manufacturing_years (car_model_id, manufacturing_year) VAL
                                                                                  (8, 2020),
                                                                                  (8, 2021),
                                                                                  (9, 2024),
-                                                                                 (9, 2025),
-                                                                                 (10, 2024),
-                                                                                 (10, 2025);
+                                                                                 (9, 2025);
 
-INSERT INTO car_model_colors (car_model_id, color) VALUES
+
+INSERT INTO car_model_colors (car_model_id, color_hexadecimal_code) VALUES
                                                        (1, '#FF0000'), -- Red
                                                        (1, '#0000FF'), -- Blue
                                                        (1, '#000000'), -- Black
@@ -55,6 +53,4 @@ INSERT INTO car_model_colors (car_model_id, color) VALUES
                                                        (8, '#FFFFFF'), -- White
                                                        (8, '#808080'), -- Gray
                                                        (9, '#C0C0C0'), -- Silver
-                                                       (9, '#0000FF'), -- Blue
-                                                       (10, '#FF0000'), -- Red
-                                                       (10, '#000000'); -- Black
+                                                       (9, '#0000FF'); -- Blue
